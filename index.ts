7c8604dfff24d80299172dacfaa2f924cab03936
anzilafatima <anzilafatima205@gmail.com>
2024-05-08 13:50:33 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+//friend checker game 
+//a game to check if someone is a friend based on there names
+
+import inquirer from "inquirer";
+
+let isFriend = await inquirer.prompt(
+    [
+        {
+            name: "name",
+            type:"string",
+            message:"Enter your friend name",
+        }
+    ]
+);
+
+
+if (isFriend.name === "anzila" || isFriend.name === "emaan"){
+    console.log(`${isFriend.name} is your friend`);
+}
+else{
+    console.log(`${isFriend.name} is not your friend.`);
+}
